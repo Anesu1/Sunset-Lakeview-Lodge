78aaad2fdf48d1ef0676a4f09d3b0bd2f1fc26de
Tawanda <tawandagdmavondo@gmail.com>
2022-03-16 23:35:42 +0200

    .

--- src/components/rooms/BookNow.js
+++ src/components/rooms/BookNow.js
@@ -1,29 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Paragraph from "../../styled/Paragraph";
-import styled from 'styled-components';
-import iconbed from '../../images/icon_bed 1.png'
-import iconpeople from '../../images/icon_people 1.png'
-import iconview from '../../images/icon_view 1.png'
-import Modal from 'react-modal';
+import styled from "styled-components";
+import iconbed from "../../images/icon_bed 1.png";
+import iconpeople from "../../images/icon_people 1.png";
+import iconview from "../../images/icon_view 1.png";
+import Modal from "react-modal";
 import { useState } from "react";
-import moment from 'moment'
+import moment from "moment";
 import Date from "../../styled/Date";
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import { BiMinusCircle, BiPlusCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import {loadStripe} from '@stripe/stripe-js';
-import {checkRoomAvailability} from '../../api/api'
+import { loadStripe } from "@stripe/stripe-js";
+import { checkRoomAvailability } from "../../api/api";
 import {
   CardElement,
   Elements,
   useStripe,
   useElements,
-} from '@stripe/react-stripe-js';
+} from "@stripe/react-stripe-js";
 import axios from "axios";
 import { baseUrl } from "../../api/api";
-
-
 
 const WrapperBook = styled.section`
   padding:5%;
@@ -82,15 +80,15 @@ const WrapperBook = styled.section`
       margin:auto 0 auto auto;
     }
     h3{
-      font-family:${props => props.theme.fam.cera};
+      font-family:${(props) => props.theme.fam.cera};
       font-size:60px;
       margin:20px 0;
       font-weight:900;
      
     }
     p{
-      font-family:${props => props.theme.fam.cera};
-      color:${props => props.theme.color.orange};
+      font-family:${(props) => props.theme.fam.cera};
+      color:${(props) => props.theme.color.orange};
       text-transform:uppercase;
       font-size:20px;
     }
@@ -99,12 +97,12 @@ const WrapperBook = styled.section`
    button{
       width:100%;
       border:none;
-      font-family:${props => props.theme.fam.oswald};
+      font-family:${(props) => props.theme.fam.oswald};
       height:50px;
       font-size:20px;
       text-transform:uppercase;
-      background:${props => props.theme.color.green};
-      color:${props => props.theme.color.orange};
+      background:${(props) => props.theme.color.green};
+      color:${(props) => props.theme.color.orange};
     }
   .icons{
     @media(min-width:768px){
@@ -117,29 +115,34 @@ const WrapperBook = styled.section`
   }
 
  
-`
+`;
 
 const FormWrapper = styled.section`
-  height:100vh;
-  width:100%;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  background:linear-gradient(to bottom, rgba(44, 65, 60, 0.3),rgba(44, 65, 60, 0.3)), url("./imgs/HP Sunsetlakeview/HP Sunsetlakeview/Mainhouse/Mainhouse.jpg");
-  background-size:cover;
-  
-  form{
-    max-width:400px;
-    background:#ffffffa2;
-    box-shadow:0px 3px 11px #00000037;
-    padding:20px;
-    border-radius:10px;
-    width:100%;
-    position:relative;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(
+      to bottom,
+      rgba(44, 65, 60, 0.3),
+      rgba(44, 65, 60, 0.3)
+    ),
+    url("./imgs/HP Sunsetlakeview/HP Sunsetlakeview/Mainhouse/Mainhouse.jpg");
+  background-size: cover;
+
+  form {
+    max-width: 400px;
+    background: #ffffffa2;
+    box-shadow: 0px 3px 11px #00000037;
+    padding: 20px;
+    border-radius: 10px;
+    width: 100%;
+    position: relative;
     .blur {
       filter: drop-shadow(0px 16px 24px rgba(17, 37, 62, 0.16));
       backdrop-filter: blur(20px);
-      z-index:-1;
+      z-index: -1;
       width: 100%;
       height: 100%;
       position: absolute;
@@ -150,93 +153,103 @@ const FormWrapper = styled.section`
     }
   }
   h1 {
-      color: #32325d;
-      font-weight: 400;
-      line-height: 50px;
-      font-size: 40px;
-      margin: 20px 0;
-      padding: 0;
-    }
-    
-    .Checkout {
-      margin: 0 auto;
-      max-width: 800px;
-      box-sizing: border-box;
-      padding: 0 5px;
-    }
-    
-    button {
-      white-space: nowrap;
-      border: 0;
-      outline: 0;
-      display: inline-block;
-      height: 40px;
-      margin-right:20px;
-      line-height: 40px;
-      padding: 0 14px;
-      box-shadow: 0 4px 6px rgba(50, 50, 93, .11), 0 1px 3px rgba(0, 0, 0, .08);
-      color: #fff;
-      border-radius: 4px;
-      font-size: 15px;
-      font-weight: 600;
-      text-transform: uppercase;
-      letter-spacing: 0.025em;
-      background-color: ${props => props.theme.color.green};
-      text-decoration: none;
-      -webkit-transition: all 150ms ease;
-      transition: all 150ms ease;
-      margin-top: 10px;
-    }
-    
-    form {
-      margin-bottom: 40px;
-      padding-bottom: 40px;
-      border-bottom: 3px solid #e6ebf1;
-    }
-    
-    button:hover {
-      color: #fff;
-      cursor: pointer;
-      background-color: ${props => props.theme.color.orange};
-      transform: translateY(-1px);
-      box-shadow: 0 7px 14px rgba(50, 50, 93, .10), 0 3px 6px rgba(0, 0, 0, .08);
-    }
-    
-    .StripeElement {
-      display: block;
-      margin: 10px 0 20px 0;
-      max-width: 500px;
-      padding: 10px 14px;
-      box-shadow: rgba(50, 50, 93, 0.14902) 0px 1px 3px, rgba(0, 0, 0, 0.0196078) 0px 1px 0px;
-      border-radius: 4px;
-      background: white;
-    }
-    
-    .StripeElement--focus {
-      box-shadow: rgba(50, 50, 93, 0.109804) 0px 4px 6px, rgba(0, 0, 0, 0.0784314) 0px 1px 3px;
-      -webkit-transition: all 150ms ease;
-      transition: all 150ms ease;
-    }
-`
+    color: #32325d;
+    font-weight: 400;
+    line-height: 50px;
+    font-size: 40px;
+    margin: 20px 0;
+    padding: 0;
+  }
+
+  .Checkout {
+    margin: 0 auto;
+    max-width: 800px;
+    box-sizing: border-box;
+    padding: 0 5px;
+  }
+
+  button {
+    white-space: nowrap;
+    border: 0;
+    outline: 0;
+    display: inline-block;
+    height: 40px;
+    margin-right: 20px;
+    line-height: 40px;
+    padding: 0 14px;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    color: #fff;
+    border-radius: 4px;
+    font-size: 15px;
+    font-weight: 600;
+    text-transform: uppercase;
+    letter-spacing: 0.025em;
+    background-color: ${(props) => props.theme.color.green};
+    text-decoration: none;
+    -webkit-transition: all 150ms ease;
+    transition: all 150ms ease;
+    margin-top: 10px;
+  }
+
+  form {
+    margin-bottom: 40px;
+    padding-bottom: 40px;
+    border-bottom: 3px solid #e6ebf1;
+  }
+
+  button:hover {
+    color: #fff;
+    cursor: pointer;
+    background-color: ${(props) => props.theme.color.orange};
+    transform: translateY(-1px);
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+  }
+
+  .StripeElement {
+    display: block;
+    margin: 10px 0 20px 0;
+    max-width: 500px;
+    padding: 10px 14px;
+    box-shadow: rgba(50, 50, 93, 0.14902) 0px 1px 3px,
+      rgba(0, 0, 0, 0.0196078) 0px 1px 0px;
+    border-radius: 4px;
+    background: white;
+  }
+
+  .StripeElement--focus {
+    box-shadow: rgba(50, 50, 93, 0.109804) 0px 4px 6px,
+      rgba(0, 0, 0, 0.0784314) 0px 1px 3px;
+    -webkit-transition: all 150ms ease;
+    transition: all 150ms ease;
+  }
+`;
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
 };
 
 // Modal.setAppElement('#yourAppElement');
 
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
-
-function BookNow({roomName, description, descriptionone, price, view, bedType, adults, room}) {
-  const {slug} = room;
+function BookNow({
+  roomName,
+  description,
+  descriptionone,
+  price,
+  view,
+  bedType,
+  adults,
+  room,
+}) {
+  const { slug } = room;
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [modalCheckout, setModalCheckOut] = React.useState(false);
@@ -250,7 +263,7 @@ function BookNow({roomName, description, descriptionone, price, view, bedType, a
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
+    subtitle.style.color = "#f00";
   }
 
   function closeModal() {
@@ -278,16 +291,22 @@ function BookNow({roomName, description, descriptionone, price, view, bedType, a
   // if (kids > 0) {
   //   setKidsage(true);
   // }
-  const Total = () =>{
-    setGuests(kids + adult)
-  }
+  const Total = () => {
+    setGuests(kids + adult);
+  };
 
- 
+// call this method when the proceed button is pressed
+  const proceed = async () => {
+    // Slug should be the room id;
+   const available = await checkRoomAvailability(startDate, endDate, slug);
+  //  Available variable should be true or false
 
+  // if false, the client should be shown a message that the room is not available for booking (already booked)
+  // if true, proceed to the checkout page passing in the total amout to the checkiut page as a parameter
+  };
 
   // const stripe = useStripe();
   // const elements = useElements();
- 
 
   // const handleSubmit = async (event) => {
   //   event.preventDefault();
@@ -302,42 +321,36 @@ function BookNow({roomName, description, descriptionone, price, view, bedType, a
   //   });
   // };
 
-  
   return (
     <WrapperBook id="details">
       <div className="book">
-      <div className="book-inner">
-        <h3>
-       
-          Great choice for a relaxing vacation for families with children or a
-          group of friends.
-        </h3>
-        <Paragraph>
-          {description}
-        </Paragraph>
-        <Paragraph>
-          {descriptionone}
-        </Paragraph>
-        <div className="icons">
+        <div className="book-inner">
+          <h3>
+            Great choice for a relaxing vacation for families with children or a
+            group of friends.
+          </h3>
+          <Paragraph>{description}</Paragraph>
+          <Paragraph>{descriptionone}</Paragraph>
+          <div className="icons">
             <div className="icon-inner">
-                <img src={iconbed} alt="" />
-                <p>{bedType}</p>
+              <img src={iconbed} alt="" />
+              <p>{bedType}</p>
             </div>
             <div className="icon-inner">
-                <img src={iconpeople} alt="" />
-                <p>{adults} Adults</p>
+              <img src={iconpeople} alt="" />
+              <p>{adults} Adults</p>
             </div>
             <div className="icon-inner">
-                <img src={iconview} alt="" />
-                <p>{view}</p>
+              <img src={iconview} alt="" />
+              <p>{view}</p>
             </div>
+          </div>
         </div>
-      </div>
-      <div className="book-item">
+        <div className="book-item">
           <p>starting from</p>
           <h3>${price}</h3>
-         <button onClick={openModal}>Book Now</button>
-      </div>
+          <button onClick={openModal}>Book Now</button>
+        </div>
       </div>
       <Modal
         isOpen={modalIsOpen}
@@ -347,51 +360,57 @@ function BookNow({roomName, description, descriptionone, price, view, bedType, a
         contentLabel="Example"
       >
         <AiOutlineCloseCircle onClick={closeModal} />
-        
+
         <div className="form" id="form">
-        <div className="nameprize">
-          <h1>{roomName}</h1>
-          <span>${price}</span>
+          <div className="nameprize">
+            <h1>{roomName}</h1>
+            <span>${price}</span>
           </div>
-        
-        <div className="blur"></div>
-        <div className="form-inner">
-          <form action="">
-            <Date
-              startDate={startDate}
-              endDate={endDate}
-              setStartDate={setStartDate}
-              setEndDate={setEndDate}
-            />
-            <div className="people">
-              <div className="people-inner">
-                <label htmlFor="">Adults <span onChange={Total}>{adult}</span></label>
-                <div className="icon" onClick={() => setAdult(adult - 1)}>
-                  <BiMinusCircle />
+
+          <div className="blur"></div>
+          <div className="form-inner">
+            <form action="">
+              <Date
+                startDate={startDate}
+                endDate={endDate}
+                setStartDate={setStartDate}
+                setEndDate={setEndDate}
+              />
+              <div className="people">
+                <div className="people-inner">
+                  <label htmlFor="">
+                    Adults <span onChange={Total}>{adult}</span>
+                  </label>
+                  <div className="icon" onClick={() => setAdult(adult - 1)}>
+                    <BiMinusCircle />
+                  </div>
+                  <div className="icon" onClick={() => setAdult(adult + 1)}>
+                    <BiPlusCircle />
+                  </div>
                 </div>
-                <div className="icon" onClick={() => setAdult(adult + 1)}>
-                  <BiPlusCircle />
+                <div className="people-inner">
+                  <label htmlFor="">
+                    Kids <span onChange={Total}>{kids}</span>
+                  </label>
+                  <div className="icon" onClick={() => setKids(kids - 1)}>
+                    <BiMinusCircle />
+                  </div>
+                  <div className="icon" onClick={() => setKids(kids + 1)}>
+                    <BiPlusCircle />
+                  </div>
                 </div>
               </div>
-              <div className="people-inner">
-                <label htmlFor="" >Kids <span onChange={Total}>{kids}</span></label>
-                <div className="icon" onClick={() => setKids(kids - 1)}>
-                  <BiMinusCircle />
-                </div>
-                <div className="icon" onClick={() => setKids(kids + 1)}>
-                  <BiPlusCircle />
-                </div>
+              <div className="total">
+                <span> Total Price: ${price + kids * 35}</span>
               </div>
-            </div>
-            <div className="total">
-            <span> Total Price: ${price + (kids*35)}</span>
-              </div>
-          </form> <button onClick={checkRoomAvailability}  className="but">Proceed</button>
+            </form>{" "}
+            <button onClick={checkRoomAvailability} className="but">
+              Proceed
+            </button>
+          </div>
         </div>
-      </div>
-     
       </Modal>
-    {/* <Modal
+      {/* <Modal
     isOpen={modalCheckout}
     onAfterOpen={afterOpenModal}
     onRequestClose={closeCheckModal}
@@ -413,10 +432,8 @@ function BookNow({roomName, description, descriptionone, price, view, bedType, a
       </FormWrapper>
   </Elements>
     </Modal> */}
-
     </WrapperBook>
   );
 }
 
 export default BookNow;
-
